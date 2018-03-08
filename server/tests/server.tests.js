210dc4996eab24224ca7b64b1b712b27e4065d2c
Ronnie <ronnie@oliservices.com>
2018-03-08 11:01:30 -0500

    Test Get

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -4,8 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+let todos = [
+    {
+        "text": "first todo"
+    },
+    {
+        "text": "second todo"
+    }
+]
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=> done());
+    Todo.remove({}).then(()=> {
+        return Todo.insertMany(todos);
+    }).then(done());
 })
 
  describe('POST /todos',()=>{
@@ -23,7 +34,7 @@ beforeEach((done)=>{
                 return done(err);
             }
 
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -42,9 +53,20 @@ beforeEach((done)=>{
                 return done(err);
             }
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e)=>done(e));
          });
+    })
+});
+describe('GET /todo',()=>{
+    it('should get all todos',(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     })
 });
